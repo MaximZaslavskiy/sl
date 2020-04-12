0cde2b2193a06e17d5655f99ba203fef9cc0d7dd
MaximZaslavskiy <maxim.zaslavskiy@gmail.com>
2020-04-12 11:22:10 +0600

    Первы модульны тест

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,18 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
+class HomePageTest(TestCase):
+    
+    def test_root_url_resolves_tohome_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    '''тест на токсичность'''
+# class SmokeTest(TestCase):
+    # '''тест на токсичность'''
     
-    def test_bad_math(self):
-        ''' тест: не правильные матемвтические расчеты '''
-        self.assertEqual(1+1,3)
+    # def test_bad_math(self):
+        # ''' тест: не правильные матемвтические расчеты '''
+        # self.assertEqual(1+1,3)
+
